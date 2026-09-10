ea7bae6a59cd6e9f9791d83a776ed60f8c30e36f
Nate Dunn <hello@natedunn.net>
2026-09-10 16:18:54 -0600

    Remove data panel fills and widen sidebar spacing

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -40,10 +40,7 @@ function TableFooter({ className, ...props }: React.ComponentProps<"tfoot">) {
   return (
     <tfoot
       data-slot="table-footer"
-      className={cn(
-        "border-t bg-muted/50 font-medium [&>tr]:last:border-b-0",
-        className
-      )}
+      className={cn("border-t font-medium [&>tr]:last:border-b-0", className)}
       {...props}
     />
   )
